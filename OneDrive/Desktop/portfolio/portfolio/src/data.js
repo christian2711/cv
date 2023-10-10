d79c10266beead4aa6539fbb5e30a27a0fbce0eb
christian1127 <distribuidoracalamuchita@gmail.com>
2023-10-10 11:26:08 -0300

    cv

--- OneDrive/Desktop/portfolio/portfolio/src/data.js
+++ OneDrive/Desktop/portfolio/portfolio/src/data.js
@@ -1,11 +1,11 @@
 
 export const skills = [
-  { name: "Java", percentage: 90 },
+  { name: "Java", percentage: 95 },
   { name: "React", percentage: 90 },
-  { name: "Node", percentage: 75 },
+  { name: "Node", percentage: 80 },
   { name: "Javascript", percentage: 85 },
   { name: "SQL", percentage: 95 },
-  { name: "CSS", percentage: 80 },
+  { name: "CSS", percentage: 85 },
   { name: "HTML", percentage: 90 },
   { name: "Spring", percentage: 85 },
   // Agrega el resto de habilidades aquí
